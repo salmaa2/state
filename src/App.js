2647fe358b2dc9af5ca167646c1289d650e6320a
salma chebbi <salmaaa.shebby@gmail.com>
2021-12-20 22:00:40 +0100

    first commi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Person from './Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+ state= { B:false }
+ toggleshow=()=>{ this.setState({ B: ! this.state.B})}
+
+  render(){
+return(
+  <div>
+    <button onClick={()=>this.toggleshow()}> {this.state.B ? 'Hide' : 'Show'} </button>
+     {this.state.B && <Person/>}
+  </div>
+)
+
+  }
 }
-
 export default App;
